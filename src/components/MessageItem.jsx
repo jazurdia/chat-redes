3e892d3bfaa09b8074b0ae1d4029d20de2ca0f1d
jazurdia <aleazurdia1@gmail.com>
2024-08-25 00:54:55 -0600

    Update MessageItem.jsx
    image preview

--- src/components/MessageItem.jsx
+++ src/components/MessageItem.jsx
@@ -1,19 +1,22 @@
-// src/components/MessageItem.jsx
 import PropTypes from 'prop-types';
-import {useContext} from 'react';
+import { useContext } from 'react';
 import AuthContext from '../auxiliaryFunctions/AuthContext.jsx';
 
-function MessageItem({ body, from, timestamp, isFile }) {
+function MessageItem({ body, from, timestamp }) {
     const { user } = useContext(AuthContext);
     const loggedUser = user.client.jid.local;
     const isSent = from.includes(loggedUser);
+
+    const isImage = body.endsWith('.png') || body.endsWith('.jpg') || body.endsWith('.jpeg') || body.endsWith('.gif');
 
     return (
         <div className={`flex ${isSent ? 'justify-end' : 'justify-start'} mb-2`}>
             <div className={`rounded-md p-2 ${isSent ? 'bg-blue-500 text-white w-3/5' : 'bg-gray-300 text-black w-3/5'}`}>
                 <p className='text-xs mb-0.5'>{from}</p>
-                {isFile ? (
-                    <img src={body} alt="file preview" className="w-full h-auto mb-0.5" />
+                {isImage ? (
+                    <a href={body} download>
+                        <img src={body} alt="file preview" className="w-full h-auto mb-0.5 cursor-pointer" />
+                    </a>
                 ) : (
                     <p className='text-md mb-0.5'>{body}</p>
                 )}
@@ -23,11 +26,12 @@ function MessageItem({ body, from, timestamp, isFile }) {
     );
 }
 
+
 export default MessageItem;
 
 MessageItem.propTypes = {
     body: PropTypes.string.isRequired,
     from: PropTypes.string.isRequired,
     timestamp: PropTypes.string.isRequired,
-    isFile: PropTypes.bool, // Añadir esta línea
+    isFile: PropTypes.bool,
 };
